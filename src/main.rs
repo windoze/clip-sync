2974169c07fc32d93d1124d0ca82092981a68eb4
Chen Xu <windoze@0d0a.com>
2023-12-29 01:58:20 +0800

    Server only build

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 #![windows_subsystem = "windows"]
 
 use clap::Parser;
-use futures::{future::join_all, TryFutureExt};
+use futures::TryFutureExt;
 use platform_dirs::AppDirs;
 use serde::{Deserialize, Serialize};
-use tokio::task::JoinHandle;
-use tray_item::{IconSource, TrayItem};
 
 use std::path::PathBuf;
 
+#[cfg(not(feature = "server-only"))]
 mod client;
+#[cfg(not(feature = "server-only"))]
 mod clipboard_handler;
+#[cfg(not(feature = "server-only"))]
 mod mqtt_client;
 
+#[cfg(not(feature = "server-only"))]
 pub use clipboard_handler::{ClipboardSink, ClipboardSource};
 
 #[derive(Debug, Clone, Default, Deserialize)]
 #[serde(deny_unknown_fields, rename_all = "kebab-case")]
 struct Args {
+    #[cfg(not(feature = "server-only"))]
     pub roles: Vec<String>,
     pub server: server::ServerConfig,
+    #[cfg(not(feature = "server-only"))]
     pub mqtt_client: mqtt_client::MqttClientConfig,
+    #[cfg(not(feature = "server-only"))]
     pub websocket_client: client::ClientConfig,
 }
 
@@ -39,69 +44,113 @@ async fn svc_main(config_path: PathBuf) -> anyhow::Result<()> {
     let config = std::fs::read_to_string(config_path)?;
     let args = toml::from_str::<Args>(&config)?;
 
-    let mut tasks: Vec<JoinHandle<anyhow::Result<()>>> = vec![];
-    if args.roles.contains(&"server".to_string()) {
-        tasks.push(tokio::spawn(async {
-            server::server_main(args.server)
-                .map_err(|e| anyhow::anyhow!("Server error: {}", e))
-                .await
-        }));
+    #[cfg(not(feature = "server-only"))]
+    {
+        let mut tasks: Vec<tokio::task::JoinHandle<anyhow::Result<()>>> = vec![];
+        if args.roles.contains(&"server".to_string()) {
+            tasks.push(tokio::spawn(async {
+                server::server_main(args.server)
+                    .map_err(|e| anyhow::anyhow!("Server error: {}", e))
+                    .await
+            }));
+        }
+        if args.roles.contains(&"mqtt-client".to_string()) {
+            tasks.push(tokio::spawn(async {
+                mqtt_client::clip_sync_svc(args.mqtt_client)
+                    .map_err(|e| anyhow::anyhow!("Mqtt client error: {}", e))
+                    .await
+            }));
+        }
+        if args.roles.contains(&"websocket-client".to_string()) {
+            tasks.push(tokio::spawn(async {
+                client::clip_sync_svc(args.websocket_client)
+                    .map_err(|e| anyhow::anyhow!("Mqtt client error: {}", e))
+                    .await
+            }));
+        }
+        if args.roles.is_empty() {
+            anyhow::bail!("No role specified");
+        }
+        for r in futures::future::join_all(tasks.into_iter())
+            .await
+            .into_iter()
+        {
+            r??;
+        }
     }
-    if args.roles.contains(&"mqtt-client".to_string()) {
-        tasks.push(tokio::spawn(async {
-            mqtt_client::clip_sync_svc(args.mqtt_client)
-                .map_err(|e| anyhow::anyhow!("Mqtt client error: {}", e))
-                .await
-        }));
+    #[cfg(feature = "server-only")]
+    {
+        server::server_main(args.server)
+            .map_err(|e| anyhow::anyhow!("Server error: {}", e))
+            .await?;
     }
-    if args.roles.contains(&"websocket-client".to_string()) {
-        tasks.push(tokio::spawn(async {
-            client::clip_sync_svc(args.websocket_client)
-                .map_err(|e| anyhow::anyhow!("Mqtt client error: {}", e))
-                .await
-        }));
-    }
-    if args.roles.is_empty() {
-        anyhow::bail!("No role specified");
-    }
-    for r in join_all(tasks.into_iter()).await.into_iter() {
-        r??;
-    }
-
     Ok(())
 }
 
-#[cfg(target_os = "macos")]
-fn get_app_icon() -> IconSource {
-    IconSource::Data {
-        width: 0,
-        height: 0,
-        data: include_bytes!("../icons/app-icon.png").to_vec(),
+#[cfg(not(feature = "server-only"))]
+mod tray {
+    use tray_item::{IconSource, TrayItem};
+
+    #[cfg(target_os = "macos")]
+    fn get_app_icon() -> IconSource {
+        IconSource::Data {
+            width: 0,
+            height: 0,
+            data: include_bytes!("../icons/app-icon.png").to_vec(),
+        }
     }
-}
 
-#[cfg(target_os = "windows")]
-fn get_app_icon() -> IconSource {
-    IconSource::Resource("default")
-}
+    #[cfg(target_os = "windows")]
+    fn get_app_icon() -> IconSource {
+        IconSource::Resource("default")
+    }
 
-#[cfg(target_os = "linux")]
-fn get_app_icon() -> IconSource {
-    let decoder = png::Decoder::new(std::io::Cursor::new(include_bytes!(
-        "../icons/app-icon.png"
-    )));
-    let mut reader = decoder.read_info().expect("Failed to decode icon");
-    let info = reader.info();
-    let mut buf = vec![0; info.raw_bytes()];
-    let output_info = reader
-        .next_frame(buf.as_mut_slice())
-        .expect("Failed to decode icon");
-    output_info.buffer_size();
+    #[cfg(target_os = "linux")]
+    fn get_app_icon() -> IconSource {
+        let decoder = png::Decoder::new(std::io::Cursor::new(include_bytes!(
+            "../icons/app-icon.png"
+        )));
+        let mut reader = decoder.read_info().expect("Failed to decode icon");
+        let info = reader.info();
+        let mut buf = vec![0; info.raw_bytes()];
+        let output_info = reader
+            .next_frame(buf.as_mut_slice())
+            .expect("Failed to decode icon");
+        output_info.buffer_size();
 
-    IconSource::Data {
-        width: output_info.width as i32,
-        height: output_info.height as i32,
-        data: buf[0..output_info.buffer_size()].to_vec(),
+        IconSource::Data {
+            width: output_info.width as i32,
+            height: output_info.height as i32,
+            data: buf[0..output_info.buffer_size()].to_vec(),
+        }
+    }
+
+    pub fn run_tray() -> anyhow::Result<()> {
+        let mut tray = TrayItem::new("ClipSync", get_app_icon())?;
+
+        #[cfg(target_os = "macos")]
+        {
+            tray.inner_mut().add_quit_item("Quit");
+            tray.inner_mut().display();
+        }
+
+        #[cfg(any(target_os = "windows", target_os = "linux"))]
+        {
+            enum Message {
+                Quit,
+            }
+            let (tx, rx) = std::sync::mpsc::sync_channel(1);
+            tray.add_menu_item("Quit", move || {
+                tx.send(Message::Quit).unwrap();
+            })?;
+            loop {
+                if matches!(rx.recv()?, Message::Quit) {
+                    log::warn!("Quit");
+                    break;
+                }
+            }
+        }
+        Ok(())
     }
 }
 
@@ -136,29 +185,10 @@ fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    let mut tray = TrayItem::new("ClipSync", get_app_icon())?;
-
-    #[cfg(target_os = "macos")]
+    #[cfg(not(feature = "server-only"))]
     {
-        tray.inner_mut().add_quit_item("Quit");
-        tray.inner_mut().display();
+        tray::run_tray()?;
     }
 
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
-    {
-        enum Message {
-            Quit,
-        }
-        let (tx, rx) = std::sync::mpsc::sync_channel(1);
-        tray.add_menu_item("Quit", move || {
-            tx.send(Message::Quit).unwrap();
-        })?;
-        loop {
-            if matches!(rx.recv()?, Message::Quit) {
-                log::warn!("Quit");
-                break;
-            }
-        }
-    }
     Ok(())
 }
